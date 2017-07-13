e94b186113aea0e421848711d74a1b9c4029ab45
DS2k2 <Jorgecoopernavarrete@gmail.com>
2017-07-13 15:48:51 -0400

    Pyg latin code

--- Pyg Latin.py
+++ Pyg Latin.py
@@ -1,7 +1,7 @@
 print 'Welcome to the Pig Latin Translator! It will automatically make the entire word lower case so heads up!'
-pyg = 'ay' #The suffix
+pyg = 'ay'  #The suffix
 
-original = raw_input('Enter a word:') #The actual word that the user enters
+original = raw_input('Enter a word:')  #The actual word that the user enters
 
 if len(original) > 0 and original.isalpha():
     word = original.lower()
@@ -10,4 +10,4 @@ if len(original) > 0 and original.isalpha():
     print new_word
 else:
     raw_input('Please enter a word.')
-    print new_word
+print new_word
