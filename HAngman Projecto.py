7c24df4d8513d24a221ace47af91e5e71a7a7fa0
DS2k2 <Jorgecoopernavarrete@gmail.com>
2017-07-14 15:48:38 -0400

    Hangman Code

--- HAngman Projecto.py
+++ HAngman Projecto.py
@@ -3,23 +3,16 @@ import numpy
 def hangman_game():
     word_list = ['genius', 'orphanage', 'torched', 'tree', 'pizza'] # The word list
     word = numpy.random.choice(word_list) # Randomly chooses a word from the word list
-    guess_letter = "" # The variable that stores the player's input
-    space = "_ " * len(word) # Represents the amount of spaces in the word
-    print space
-    stage = 0
-    while stage <= 6:
-        while guess_letter == "": # Makes it so you have to input a letter
-            guess_letter = raw_input("Enter a letter please:")
-        l_list = guess_letter[0] # The list that stores the letters guessed
-        for letter in word: # This for for loop checks if the guessed letter is in the word and then replaces the corresponding space
-            if guess_letter[0] == word[0]:
-                print guess_letter[0] + space[1:]
-                print 'Good job! You have guessed:' + guess_letter[0]
-            break
+    word_len=[]
+    n=-1
 
-        else: # Prints the gallows with the partially created hanging man
+    for letter in word:
+        word_len.append("_" )# Represents the amount of spaces in the word
+    stage = 0
+    def hangman_stage(stage):
+        if stage ==0:
             print "  |------|"
-            print "  |      0"
+            print "  |       "
             print "  |"
             print "  |"
             print "  |"
@@ -28,8 +21,113 @@ def hangman_game():
             print "  |              |"
             print "Good guess! Try again!"
             print "You have guessed:" + l_list
+        elif stage == 1:
+            print "  |------|"
+            print "  |      O"
+            print "  |"
+            print "  |"
+            print "  |"
+            print "  |"
+            print "  |--------------|"
+            print "  |              |"
+            print "Good guess! Try again!"
+            print "You have guessed:" + l_list
+        elif stage ==2:
+            print "  |------|"
+            print "  |      O"
+            print "  |      |"
+            print "  |"
+            print "  |"
+            print "  |"
+            print "  |--------------|"
+            print "  |              |"
+            print "Good guess! Try again!"
+            print "You have guessed:" + l_list
+        elif stage == 3:
+            print "  |------|"
+            print "  |      O"
+            print "  |     _|"
+            print "  |"
+            print "  |"
+            print "  |"
+            print "  |--------------|"
+            print "  |              |"
+            print "Good guess! Try again!"
+            print "You have guessed:" + l_list
+        elif stage == 4:
+            print "  |------|"
+            print "  |      O"
+            print "  |     _|_"
+            print "  |"
+            print "  |"
+            print "  |"
+            print "  |--------------|"
+            print "  |              |"
+            print "Good guess! Try again!"
+            print "You have guessed:" + l_list
+        elif stage == 5:
+            print "  |------|"
+            print "  |      O"
+            print "  |     _|_"
+            print "  |      |"
+            print "  |"
+            print "  |"
+            print "  |--------------|"
+            print "  |              |"
+            print "Good guess! Try again!"
+            print "You have guessed:" + l_list
+        elif stage == 6:
+            print "  |------|"
+            print "  |      O"
+            print "  |     _|_"
+            print "  |      |"
+            print "  |     /"
+            print "  |"
+            print "  |--------------|"
+            print "  |              |"
+            print "Good guess! Try again!"
+            print "You have guessed:" + l_list
+        elif stage == 7:
+            print "  |------|"
+            print "  |      O"
+            print "  |     _|_"
+            print "  |      |"
+            print "  |     / \\"
+            print "  |"
+            print "  |--------------|"
+            print "  |              |"
+            game = ''
+            while game == "":
+                game = raw_input('Would you like to play hangman? y or n:')
+            if game == 'y':
+                hangman_game()
 
-
+    print word_len
+    while stage < 7:
+        guess_letter = ""
+        while guess_letter == "": # Makes it so you have to input a letter
+            guess_letter = raw_input("Enter a letter please:")
+        l_list = guess_letter[0] # The list that stores the letters guessed
+        for l in word: # This for loop checks if the guessed letter is in the word and then replaces the corresponding space
+            if guess_letter[0] in word:
+                word_len[n]=guess_letter
+                print word_len
+                print 'Good job! You have guessed:' + l_list
+                break
+            else:
+                print "  |------|"
+                print "  |      O"
+                print "  |"
+                print "  |"
+                print "  |"
+                print "  |"
+                print "  |--------------|"
+                print "  |              |"
+                print "Good guess! Try again!"
+                print "You have guessed:" + l_list
+                stage=+1
+                break
+        l_list = l_list + guess_letter
 
 intro = ""
 while intro == "":
